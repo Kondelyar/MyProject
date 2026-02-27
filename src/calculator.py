273e9c3876c9e5183711ecfcf720dae1f9421f01
Kondelyar <mishavovkogonto@gmail.com>
2026-02-27 19:10:35 +0200

    Trigger linting for screenshots

--- src/calculator.py
+++ src/calculator.py
@@ -1,9 +1,7 @@
 class Calculator:
-    def add(self, a, b):
-        return a + b
-
-    def divide(self, a, b):
-        if b == 0:
-            raise ValueError("Division by zero!")
-        return a / b
-
+    def add(self,a,b): # Помилка E231 (немає пробілів після ком)
+        return a+b # Помилка E225 (немає пробілів навколо +)
+    def divide(self, a, b): # Помилка E302 (має бути 2 порожніх рядки перед методом)
+        if b==0: # Помилка E225 (немає пробілів навколо ==)
+            raise ValueError('Division by zero!') # Black порадить змінити одинарні лапки на подвійні
+        return a/b
